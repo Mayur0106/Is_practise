ce16e07a1ec8feb66b81363a73564b51dd39ab14
Mayur <mayurshinde0106@gmail.com>
2022-04-19 15:09:57 +0530

    tenth commit

---
+++ large_world_in_string.py
@@ -0,0 +1,10 @@
+strr="India is the seventh-largest country by area and the second-most populous country in the world, situated in Asia"
+a=strr.split()
+max1=len(a[0])
+temp=a[0]
+
+for i in a:
+    if(len(i)>max1):
+        temp=i
+print(temp)
+print(len(temp))
