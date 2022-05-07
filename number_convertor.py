4dd5dfe68be093f0d2e12ce47d16172d9bea30a8
Mayur <mayurshinde0106@gmail.com>
2022-05-07 10:29:02 +0530

    first commit

---
+++ number_convertor.py
@@ -0,0 +1,18 @@
+def convert():
+    num=int(input("Enter the number : "))
+    l=len(bin(num))
+    print("Bin".center(l),"   ","oct".center(l),"   ","dec".center(l),"   ","Hex".center(l))
+    for i in range(num):
+        m=bin(i).replace("0b",' ')
+        n=oct(i).replace("0o"," ")
+        o=hex(i).replace("0x",' ')
+        ii=str(i)
+        
+        print(m.rjust(l),"   ",n.rjust(len(bin(num))),"    ",ii.rjust(l),"    ",o.rjust(l))
+       
+while(True):
+    print(" You can continue the program(Yes/No) \n\t1.Yes \n\t2.NO ")
+    ch=input("Enter the your chise :")
+    if(ch==1 or 'Yes' or 'yes'):
+        print("add")
+        convert()
--- string_conversion .py
+++ string_conversion .py
@@ -1,10 +1,11 @@
-num=input("Enter the ")
+num="JSpm icoEr is the one OF th e&& bigGeST cOLLEge in puNE ! "
+#num=input("Enter the ")
 l1=''
-for i in num:
-    if(i>='a' and i<='z'):
-        l1=l1+i.upper()
-    elif(i>='A' and i<='Z'):
-        l1=l1+i
+for i in range (0,len(num)):
+    if(num[i]>='a' and num[i]<='z'):
+        l1=l1+num[i].upper()
+    elif(num[i]>='A' and num[i]<='Z'):
+        l1=l1+num[i].lower()
     else:
-        l1=l1+'_-'
+        l1=l1+'_'
 print(l1)
