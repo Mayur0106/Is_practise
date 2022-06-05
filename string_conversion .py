def6a75122c00a720127ace63461fa42c298237b
Mayur  Shinde <97580421+Mayur0106@users.noreply.github.com>
2022-06-05 22:57:08 +0530

    Update string_conversion .py

--- string_conversion .py
+++ string_conversion .py
@@ -6,7 +6,7 @@ num=input("Enter the string : ")
 #>>>>>> 9c33794ec8331ca9633e3f102581f0a7a647b6f6
 l1=''
 for i in range (0,len(num)):
-    if(num[i]>='a' and num[i]<='z'):
+    if(num[i]>='a' and num[i]<='z' ):
         l1=l1+num[i].upper()
     elif(num[i]>='A' and num[i]<='Z' ):
         l1=l1+num[i].lower()
