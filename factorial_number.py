aaae4965661434279a0c385671d8476323218094
Mayur  Shinde <97580421+Mayur0106@users.noreply.github.com>
2022-06-16 18:54:05 +0530

    Update factorial_number.py

--- factorial_number.py
+++ factorial_number.py
@@ -2,5 +2,5 @@ num= int(input("Enter the numbere : "))
 list=1
 for i in range(2,num+1 ):
     list=list*i
-print(f"{list} ia your answer  .")
+print(f"{list} ia your answer  .  ")
 print("Thanks.....!  ")
